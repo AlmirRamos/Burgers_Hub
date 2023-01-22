a4d005ab48168c79e146533802894b056fec3532
Arley Santana <arley1995@ucl.br>
2023-01-22 11:51:06 -0300

    Estruturando base do projeto

--- app/src/main/java/com/example/burgershub/util/Extensions.kt
+++ app/src/main/java/com/example/burgershub/util/Extensions.kt
@@ -1,2 +1,25 @@
 package com.example.burgershub.util
 
+import com.google.gson.Gson
+import retrofit2.HttpException
+import java.text.DecimalFormat
+import java.text.DecimalFormatSymbols
+import java.text.NumberFormat
+import java.util.*
+
+inline fun <reified T> HttpException.getErrorResponse(): T? {
+    return try {
+        Gson().fromJson(response()?.errorBody()?.string(), T::class.java)
+    } catch (e: Exception) {
+        null
+    }
+}
+
+fun Float.formattedValue(): String? {
+    val nf: NumberFormat = DecimalFormat(
+        "#,##0.00", DecimalFormatSymbols(
+            Locale("pt", "BR")
+        )
+    )
+    return nf.format(this)
+}
