17e0c683ff77b3479dd86e2ce2df26a9989ab4b6
Almir Ramos <ardsdev23@gmail.com>
2024-02-09 19:55:25 -0300

    Criando objeto da camada Data

--- app/src/main/java/com/example/burgershub/network/ServiceProvider.kt
+++ app/src/main/java/com/example/burgershub/network/ServiceProvider.kt
@@ -1,5 +1,24 @@
 package com.example.burgershub.network
 
-class ServiceProvider {
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
+class ServiceProvider
+{
+    private val baseUrl = ""
+
+    private val  client = OkHttpClient.Builder()
+        .connectTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(baseUrl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(client)
+        .build()
+
+    fun <API> creatService(apiClass: Class<API>) : API = retrofit.create(apiClass)
 }
